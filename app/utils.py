23e03b6ca448e1237b499533c04e1f8d874821d3
Greatsol <ilia2309ilia@gmail.com>
2022-09-23 03:42:56 +0300

    [add]: file writers and file parsers

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,12 @@
 import json
 import re
 from queue import Queue
-from typing import Any, Generator
+from typing import Any, Generator, Optional
+from pathlib import Path
 
 import pandas as pd
 
-from app.config import FILES_PATH
+from app.config import FILES_PATH, JSON_PATH
 from app.parser import Parser
 from app.pydantic_classes import Game, Games
 from app.main import logger
@@ -56,13 +57,22 @@ def parse_user_id_by_category(product_type_id: int, parser: Parser) -> list[int]
     page_counter = 1
     content_length = 20
     users_id = []
-    while content_length == 20:
+    error_counter = 0
+    while content_length == 20 and error_counter < 5:
         data["pn"] = page_counter
         response = parser.request("POST", url, data=str(data))
+        if response == False:
+            error_counter += 1
+            continue
         try:
-            users = response.json()["content"]
+            response = response.json()
+            if response["status"] == "ERROR":
+                error_counter += 1
+                continue
+            users = response["content"]
             content_length = len(users)
             users_id.extend(user["userId"] for user in users)
+            error_counter = 0
         except:
             logger.error("Пользователи не считались из категории.")
         page_counter += 1
@@ -87,3 +97,23 @@ def write_last_file(q: Queue) -> None:
     while q.qsize():
         data.append(q.get())
     write_gamers_data_to_file(data, file_name="final")
+
+
+def write_ids_to_json(data: set[int]) -> None:
+    """Записать все id в json для продолжения парсинга"""
+    with open(JSON_PATH, "w", encoding="utf8") as file:
+        json.dump(list(data), file, indent=4)
+
+
+def cocncat_xlsxs(file_path: Optional[str] = None) -> Optional[pd.DataFrame]:
+    """Объединяет все xlsx в FILE_PATH."""
+    df = pd.concat([pd.read_excel(path) for path in Path(FILE_PATH).iterdir()])
+    if file_path:
+        df.to_excel(file_path)
+    else:
+        return df
+
+
+def parse_used_id_from_xlsx() -> set[int]:
+    """Возвращает список использованных id."""
+    return set(cocncat_xlsxs().user_id)
