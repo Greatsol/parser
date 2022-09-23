bf5a0f18cc412eae8f28d9eec413e5a464072c72
Greatsol <ilia2309ilia@gmail.com>
2022-09-23 03:47:11 +0300

    [add]: continue parsing mode

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 TOR_PORT = 9050
 PROXY_LIST = ""
 THREADS_NUM = 9
-FILES_PATH = "~/data/"
+FILES_PATH = "/root/data/"
 FILE_ROTATION = 1000
+JSON_PATH = "/root/backup/ids.json"
+CONTINUE_PARSING = True
--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from typing import Any
 
 from loguru import logger
 
-from app.config import THREADS_NUM, FILES_PATH, FILE_ROTATION
+from app.config import THREADS_NUM, FILES_PATH, FILE_ROTATION, CONTINUE_PARSING
 from app.parse_types import Gamer
 from app.parser import Parser
 from app.pydantic_classes import Game
@@ -14,6 +14,8 @@ from app.utils import (
     parse_categories_id,
     parse_user_id_by_category,
     write_gamers_data_to_file,
+    write_ids_to_json,
+    parse_used_id_from_xlsx
 )
 
 
@@ -27,21 +29,25 @@ logger.add(
 
 
 USERS_ID_DATA = []
+CATEGORY_QUEUE: Queue[Game] = Queue()
 MAIN_QUEUE: Queue[int] = Queue()
 USER_DATA_QUEUE: Queue[dict[str, Any]] = Queue()
 
 
 @logger.catch
-def parse_id_thread(games: list[Game], name: str) -> None:
+def parse_id_thread(name: str) -> None:
     """Поток парсинга id игороков из категорий."""
     logger.info(f"Поток {name} запущен.")
     parser = Parser()
-    for counter, game in enumerate(games):
+    counter = 1
+    while CATEGORY_QUEUE.qsize():
+        game = CATEGORY_QUEUE.get()
         gamers_ids = parse_user_id_by_category(game.id, parser)
         logger.info(
-            f"Поток {name}: Id игроков {game.name} {game.id} спаршены. Игроков:{len(gamers_ids). {counter+1}/{len(games)}}"
+            f"Поток {name}: Id игроков {game.name} {game.id} спаршены. Игроков:{len(gamers_ids)}. [{counter}]"
         )
         USERS_ID_DATA.extend(gamers_ids)
+        counter += 1
     logger.info(f"Поток {name} завершён.")
 
 
@@ -73,24 +79,33 @@ def write_data_thread() -> None:
             sleep(300)
 
 
-def main():
-    logger.info("Начало парсинга.")
+def parse_users_ids() -> None:
     games = parse_categories_id(parser=Parser())
     if isinstance(games, Exception):
         raise games
-    chunks = func_chunks_generators(games, THREADS_NUM)
+    
+    for game in games:
+        CATEGORY_QUEUE.put(game)
     category_threads = [
         Thread(
-            target=parse_id_thread, args=(chunk, f"Категории {i}"), name=f"Категории {i}"
+            target=parse_id_thread, kwargs={"name": f"Категории {i}"}, name=f"Категории {i}"
         )
-        for i, chunk in enumerate(chunks)
+        for i in range(1, THREADS_NUM+1)
     ]
     [thread.start() for thread in category_threads]
     [thread.join() for thread in category_threads]
 
     global USERS_ID_DATA
+
+    write_ids_to_json(set(USERS_ID_DATA))
+
+    if CONTINUE_PARSING:
+        USERS_ID_DATA = list(set(CONTINUE_PARSING) / parse_used_id_from_xlsx)
+    else:
+        pass
     logger.info(f"Из категорий спаршено {len(USERS_ID_DATA)} id.")
     USERS_ID_DATA = set(USERS_ID_DATA)
+    write_ids_to_json(USERS_ID_DATA)
     logger.info(f"Уникальных id для парсинга {len(USERS_ID_DATA)}.")
 
     for user_id in USERS_ID_DATA:
@@ -98,11 +113,18 @@ def main():
 
     logger.info(f"Очередь создана. {MAIN_QUEUE.qsize()=}")
 
+
+@logger.catch
+def main():
+    logger.info("Начало парсинга.")
+
+    parse_users_ids()
+    
     user_parsers = []
     for i in range(THREADS_NUM):
         name = f"Парсинг пользователей {i}"
         user_parsers.append(
-            Thread(target=parse_user_data, args=(name), name=name))
+            Thread(target=parse_user_data, kwargs={"name": name}, name=name))
     [thread.start() for thread in user_parsers]
 
     write_thread = Thread(target=write_data_thread, name="Поток записи")
@@ -110,3 +132,5 @@ def main():
 
     [thread.join() for thread in user_parsers]
     write_thread.join()
+
+    logger.info("Парсинг завершён.")
